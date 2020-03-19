e6c4cedac4262b720b2a4655858a80db586f7ab6
Alina_Stepanova <stepanovaalina15@gmail.com>
2020-03-19 20:48:27 +0200

    set up data binding

--- app/src/main/java/com/avs/rates/MainActivity.kt
+++ app/src/main/java/com/avs/rates/MainActivity.kt
@@ -2,11 +2,20 @@ package com.avs.rates
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.avs.rates.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.mainViewModel = viewModel
     }
 }
---
+++ app/src/main/java/com/avs/rates/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.avs.rates
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
