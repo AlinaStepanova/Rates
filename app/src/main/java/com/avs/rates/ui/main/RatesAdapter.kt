6aa1d98a1e44ce510d3d83fe4f8a2a5e3d51c0aa
Alina_Stepanova <stepanovaalina15@gmail.com>
2020-03-28 20:09:27 +0200

    incapsulete methods in rates view holder

--- app/src/main/java/com/avs/rates/ui/main/RatesAdapter.kt
+++ app/src/main/java/com/avs/rates/ui/main/RatesAdapter.kt
@@ -18,7 +18,8 @@ interface ListItemClickListener {
     fun onListItemClick(clickedItemIndex: Int)
 }
 
-class RatesAdapter(rates: ArrayList<BaseCurrency>?) : RecyclerView.Adapter<RatesAdapter.RatesViewHolder>() {
+class RatesAdapter(rates: ArrayList<BaseCurrency>?) :
+    RecyclerView.Adapter<RatesAdapter.RatesViewHolder>() {
 
     private lateinit var context: Context
 
@@ -30,25 +31,19 @@ class RatesAdapter(rates: ArrayList<BaseCurrency>?) : RecyclerView.Adapter<Rates
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RatesViewHolder {
         context = parent.context
-        val layoutForListItems: Int = R.layout.rate_item_list
-        val inflater = LayoutInflater.from(context)
-        val view = inflater.inflate(layoutForListItems, parent, false)
-
-        return RatesViewHolder(view)
+        return RatesViewHolder.from(parent, context)
     }
 
     override fun getItemCount() = currencies?.size ?: 0
 
     override fun onBindViewHolder(holder: RatesViewHolder, position: Int) {
-        holder.bindRate(currencies?.get(position)?.rate.toString())
-        holder.bindCurrencyFullName(currencies?.get(position)?.getFullName()?.let {
-            context.getString(it)
-        })
-        holder.bindCurrencyShortName(currencies?.get(position)?.getShortName())
-        holder.bindFlagImage(currencies?.get(position)?.getImagePath())
+        val item = currencies?.get(position)
+        if (item != null) {
+            holder.bind(item, context)
+        }
     }
 
-    inner class RatesViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+    class RatesViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
 
         private var currencyShortName: TextView = view.findViewById(R.id.tvCurrencyShortName)
         private var currencyFullName: TextView = view.findViewById(R.id.tvCurrencyFullName)
@@ -59,19 +54,26 @@ class RatesAdapter(rates: ArrayList<BaseCurrency>?) : RecyclerView.Adapter<Rates
             view.setOnClickListener(this)
         }
 
-        fun bindCurrencyShortName(name: String?) {
+        fun bind(item: BaseCurrency, context: Context) {
+            bindRate(item.rate.toString())
+            bindCurrencyFullName(item.getFullName().let { context.getString(it) })
+            bindCurrencyShortName(item.getShortName())
+            bindFlagImage(item.getImagePath())
+        }
+
+        private fun bindCurrencyShortName(name: String?) {
             if (name != null) currencyShortName.text = name
         }
 
-        fun bindCurrencyFullName(name: String?) {
+        private fun bindCurrencyFullName(name: String?) {
             if (name != null) currencyFullName.text = name
         }
 
-        fun bindRate(value: String) {
+        private fun bindRate(value: String) {
             rate.setText(value)
         }
 
-        fun bindFlagImage(imageUrl: String?) {
+        private fun bindFlagImage(imageUrl: String?) {
             Picasso.get()
                 .load(imageUrl)
                 .fit()
@@ -82,6 +84,15 @@ class RatesAdapter(rates: ArrayList<BaseCurrency>?) : RecyclerView.Adapter<Rates
         override fun onClick(view: View) {
             val clickedPosition = adapterPosition
             //onClickListener.onListItemClick(clickedPosition)
+        }
+
+        companion object {
+            fun from(parent: ViewGroup, context: Context): RatesViewHolder {
+                val layoutForListItems: Int = R.layout.rate_item_list
+                val inflater = LayoutInflater.from(context)
+                val view = inflater.inflate(layoutForListItems, parent, false)
+                return RatesViewHolder(view)
+            }
         }
 
     }
